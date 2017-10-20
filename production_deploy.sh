8fe68d11b0ecd699b54ba9f636d0fcd10cf4699e
Andres <afcadiz@uc.cl>
2017-10-20 17:52:53 -0300

    added  model deployment scripts

--- production_deploy.sh
+++ production_deploy.sh
@@ -2,4 +2,4 @@
 
 scp -i "/home/ironcadiz/.ssh/gc_p" -r newton/data 35.185.42.34:/home/felipe/tuniversidad-scores-ms/newton/data
 scp -i "/home/ironcadiz/.ssh/gc_p" -r newton/forest/serialized 35.185.42.34:/home/felipe/tuniversidad-scores-ms/newton/forest/serialized
-scp -i "/home/ironcadiz/.ssh/gc_p" -r newton/knn/serialized 35.185.42.34:/home/felipe/tuniversidad-scores-ms/newton/knn/serialized#!/usr/bin/env bash
+scp -i "/home/ironcadiz/.ssh/gc_p" -r newton/knn/serialized 35.185.42.34:/home/felipe/tuniversidad-scores-ms/newton/knn/serialized
