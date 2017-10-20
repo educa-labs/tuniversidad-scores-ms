631d82301a65d47e9c855f6aa7463fe01f5f6c92
Andres <afcadiz@uc.cl>
2017-10-20 17:50:04 -0300

    added  model deployment scripts

---
+++ development_deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+scp -r newton/data hielo@newton.tuniversidad.cl:/home/hielo/tuniversidad-scores-ms/newton/data
+scp -r newton/forest/serialized hielo@newton.tuniversidad.cl:/home/hielo/tuniversidad-scores-ms/newton/forest/serialized
+scp -r newton/knn/serialized hielo@newton.tuniversidad.cl:/home/hielo/tuniversidad-scores-ms/newton/knn/serialized
